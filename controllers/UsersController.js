e4c646e82a017ffaa200e790f88e337f9d0b8a9d
aungUknow <thantaungnyi@gmail.com>
2023-03-23 15:32:52 +0630

    Change res

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -20,8 +20,7 @@ const registerUser = async function(req, res, next) {
         let user = await userService.register(userName, password, email, age, money, totalMoney, type);
         let payload = { id: user._id };
         const token = jwt.sign(payload, config.TOKEN_SECRET);
-        res.send("Username",userName,"email",email)
-        res.status(200).send({ token });
+        res.status(200).send({ token }, user);
     } catch (err) {
         console.log(err)
         res.status(400).send({ message: "User already existed" });
